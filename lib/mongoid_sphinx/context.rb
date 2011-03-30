ade3b0ffed0c3f71df9c45ddd48590c5dd846197
Khaled Agrama <kagrama@gmail.com>
2011-03-29 22:49:03 -0400

    Don't suppress NameErrors, otherwise you get silent failure when there's a problem with your model class load (e.g. broken search_index call)

--- lib/mongoid_sphinx/context.rb
+++ lib/mongoid_sphinx/context.rb
@@ -1,36 +1,36 @@
 class MongoidSphinx::Context
   attr_reader :indexed_models
-  
+
   def initialize(*models)
     @indexed_models = []
   end
-  
+
   def prepare
     MongoidSphinx::Configuration.instance.indexed_models.each do |model|
       add_indexed_model model
     end
-    
+
     return unless indexed_models.empty?
-    
+
     load_models
   end
-  
+
   def define_indexes
     indexed_models.each { |model|
       model.constantize.define_indexes
     }
   end
-  
+
   def add_indexed_model(model)
     model = model.name if model.is_a?(Class)
-    
+
     indexed_models << model
     indexed_models.uniq!
     indexed_models.sort!
   end
-  
+
   private
-  
+
   # Make sure all models are loaded - without reloading any that
   # ActiveRecord::Base is already aware of (otherwise we start to hit some
   # messy dependencies issues).
@@ -39,18 +39,16 @@ class MongoidSphinx::Context
     MongoidSphinx::Configuration.instance.model_directories.each do |base|
       Dir["#{base}**/*.rb"].each do |file|
         model_name = file.gsub(/^#{base}([\w_\/\\]+)\.rb/, '\1')
-        
+
         next if model_name.nil?
         next if defined?(::ActiveRecord::Base) && ::ActiveRecord::Base.send(:descendants).detect { |model|
           model.name == model_name.camelize
         }
-        
+
         begin
           model_name.camelize.constantize
         rescue LoadError
           model_name.gsub!(/.*[\/\\]/, '').nil? ? next : retry
-        rescue NameError
-          next
         rescue StandardError => err
           STDERR.puts "Warning: Error loading #{file}:"
           STDERR.puts err.message
